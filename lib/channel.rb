913131dbb486bb4b66b3fdccb68b8b31e7ebc29b
Dan Roberts <dan@adadevelopersacademy.org>
2017-10-31 10:15:31 -0700

    Test the Channel class

--- lib/channel.rb
+++ lib/channel.rb
@@ -1,5 +1,5 @@
 class Channel
-  attr_reader :name, :id, :purpose, :is_archived, :members
+  attr_reader :name, :id, :purpose, :is_archived, :is_general, :members
 
   def initialize(name, id, options = {} )
     raise ArgumentError if name == nil || name == "" || id == nil || id == ""
@@ -9,7 +9,7 @@ class Channel
 
     @purpose = options[:purpose]
     @is_archived = options[:is_archived]
-    @is_general = options[:is_archived]
+    @is_general = options[:is_general]
     @members = options[:members]
   end
 end
--- test/lib/channel_test.rb
+++ test/lib/channel_test.rb
@@ -2,8 +2,47 @@ require 'test_helper'
 
 describe Channel do
 
-  it "This test needs to fail" do
-    false.must_equal true
+  it "Can be instantiated with a name and ID" do
+    Channel.new("name", "id")
   end
+
+  it "Requires a name and ID" do
+    proc {
+      Channel.new()
+    }.must_raise ArgumentError
+
+    proc {
+      Channel.new("name")
+    }.must_raise ArgumentError
+
+    # Empty strings are not permitted either
+    proc {
+      Channel.new("", "")
+    }.must_raise ArgumentError
+  end
+
+  it "Tracks name and ID" do
+    name = "test_name"
+    id = "test_id"
+    chan = Channel.new(name, id)
+    chan.name.must_equal name
+    chan.id.must_equal id
+  end
+
+  it "Tracks optional args" do
+    options = {
+      purpose: "test_purpose",
+      is_archived: "test_archived",
+      is_general: "test_general",
+      members: "test_members"
+    }
+    chan = Channel.new("name", "id", options)
+    
+    chan.purpose.must_equal options[:purpose]
+    chan.is_archived.must_equal options[:is_archived]
+    chan.is_general.must_equal options[:is_general]
+    chan.members.must_equal options[:members]
+  end
+
 
 end
