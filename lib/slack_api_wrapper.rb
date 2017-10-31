1394c4a9e68dc8a89079e37d0f7f9eff8558e86c
Dan Roberts <dan@adadevelopersacademy.org>
2017-10-31 09:52:07 -0700

    Redo the initial rails setup and all the work for getting channels working in a new Rails project with the minitest spec configured

---
+++ lib/slack_api_wrapper.rb
@@ -0,0 +1,49 @@
+# require `httparty`
+
+class SlackApiWrapper
+  BASE_URL = "https://slack.com/api/"
+  TOKEN = ENV["SLACK_TOKEN"]
+
+  def self.list_channels
+    url = BASE_URL + "channels.list?" + "token=#{TOKEN}" + "&pretty=1&exclude_archived=1"
+    data = HTTParty.get(url)
+    channel_list = []
+    if data["channels"]
+      data["channels"].each do |channel_data|
+        channel_list << self.create_channel(channel_data)
+      end
+    end
+    return channel_list
+  end
+
+  def self.send_msg(channel, msg)
+    puts "Sending message to channel #{channel}: #{msg}"
+
+    url = BASE_URL + "chat.postMessage?" + "token=#{TOKEN}"
+
+    response = HTTParty.post(url,
+    body:  {
+      "text" => "#{msg}",
+      "channel" => "#{channel}",
+      "username" => "Roberts-Robit",
+      "icon_emoji" => ":robot_face:",
+      "as_user" => "false"
+    },
+    :headers => { 'Content-Type' => 'application/x-www-form-urlencoded' })
+    return response.success?
+  end
+
+  private
+
+  def self.create_channel(api_params)
+    return Channel.new(
+      api_params["name"],
+      api_params["id"],
+      {
+        purpose: api_params["purpose"],
+        is_archived: api_params["is_archived"],
+        members: api_params["members"]
+      }
+    )
+  end
+end
---
+++ test/lib/channel_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+describe Channel do
+
+  it "This test needs to fail" do
+    false.must_equal true
+  end
+
+end
